e1acabb01041af83ed78b21be3b2c0838f77da88
Jhon Jairo Tumiri <jhon.jairo.tumiri@gmail.com>
2024-02-17 17:09:30 -0300

    add category

--- index.js
+++ index.js
@@ -11,6 +11,37 @@ form_data.addEventListener("submit", (e) => {
     console.log(cat.value, cat.name);
     copy_category.innerHTML += `"${cat.value}",`;
     list_categoria.innerHTML += `<li>${cat.value}</li>`;
+
+    console.log(
+      JSON.stringify({
+        category: cat.value,
+      })
+    );
+    let req = new XMLHttpRequest();
+    req.onreadystatechange = () => {
+      if (req.readyState == XMLHttpRequest.DONE) {
+        console.log(req.responseText);
+      }
+    };
+
+    req.open("POST", "https://api.jsonbin.io/v3/b", true);
+    req.setRequestHeader("Content-Type", "application/json");
+    req.setRequestHeader(
+      "X-Master-Key",
+      "$2a$10$HSXqh33JY5Kov.6XF/iXBeC420WH9fOKfXa8erY.DSDbMzXhlqyKy"
+    );
+    req.setRequestHeader(
+      "X-Access-key",
+      "$2a$10$vsvVkMi5kJlVy8oupAvPXOKAhO7XrJqtaG3OLfEAr/fvdWYYPE3ge"
+    );
+    req.setRequestHeader("X-Bin-Private", "false");
+    req.setRequestHeader("X-Collection-Name", "category");
+    req.setRequestHeader("X-Collection-Id", "65d10849dc74654018a62b52");
+    req.send(
+      JSON.stringify({
+        category: cat.value,
+      })
+    );
     cat.value = "";
   } else {
     alert_category.style.display = "block";
